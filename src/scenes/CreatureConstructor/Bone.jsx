04c6a438407ce8b16770add0e973ce7565b01939
Azzaid <johanas.azzaid@gmail.com>
2018-09-24 08:46:58 +0300

    Bone angle for bone creation meckanic
    Bone angle for bone creation meckanic

--- src/scenes/CreatureConstructor/Bone.jsx
+++ src/scenes/CreatureConstructor/Bone.jsx
@@ -9,29 +9,22 @@ export default class Bone {
     this.index = index;
     this.engine = engine;
     this.handleBoneClick = handleBoneClick(index);
-
-
-    this.initialLenght = Math.sqrt(Math.pow((this.joints[0].currentY-this.joints[1].currentY) ,2) + Math.pow((this.joints[0].currentX-this.joints[1].currentX),2));
-
-    this.coordX = this.joints[0].currentX+(jointWith/2);
-    this.coordY = this.joints[0].currentY+(jointHeight/2)-(boneHeigth/2);
-    this.middleX = this.coordX + (this.joints[1].currentX-this.joints[0].currentX)/2;
-    this.middleY = this.coordY + (this.joints[1].currentY-this.joints[0].currentY)/2;
-    this.lenght = Math.sqrt(Math.pow((this.joints[0].currentY-this.joints[1].currentY) ,2) + Math.pow((this.joints[0].currentX-this.joints[1].currentX),2));
-    this.angle = getAngleBetweenDots([this.joints[0].currentX,this.joints[0].currentY],[this.joints[1].currentX,this.joints[1].currentY]);
-
-
-    this.matterObject = Matter.Bodies.rectangle(x, y, width, height, [options])
-      this.initialX, this.initialY, JOINT.radius*2,
-      {
-        density: 0.04,
-        friction: 0.01,
-        frictionAir: 0.00001,
-        restitution: 0.8,
-      });
+    
+    this.lenght = Math.sqrt(Math.pow((this.joint1.y-this.joint2.y) ,2) + Math.pow((this.joint1.x-this.joint2.x),2));
+    this.angle = getAngleBetweenDots([this.joints[0].currentX,this.joint1.y],[this.joints[1].currentX,this.joint2.y]);
+    
+    this.x = this.joint1.y - bone.thickness;
+    this.y = this.joints[0].currentY+(jointHeight/2)-(boneHeigth/2);
+    
+    this.matterObject = Matter.Bodies.rectangle(x, y, width, height, {
+      density: 0.04,
+      friction: 0.01,
+      frictionAir: 0.00001,
+      restitution: 0.8,
+    });
     this.constraintWithJoint1 = Matter.Constraint.create({bodyA:joint1, bodyB:this.matterObject, stiffness:1});
     this.constraintWithJoint2 = Matter.Constraint.create({bodyA:joint2, bodyB:this.matterObject, stiffness:1});
-    Matter.World.add(this.engine.world, [this.constraintWithJoint1, this.constraintWithJoint2, this.matterObject]);
+    Matter.World.add(this.engine.world, [this.matterObject, this.constraintWithJoint1, this.constraintWithJoint2]);
   }
 
   render() {
